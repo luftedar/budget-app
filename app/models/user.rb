ceae93bcdf8acc3543d0068c91afb395485fd90a
luftedar <ugurorcun91@gmail.com>
2022-03-20 16:48:46 +0300

    Add Validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :groups, dependent: :destroy
   has_many :expenses, dependent: :destory
+
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
 end
