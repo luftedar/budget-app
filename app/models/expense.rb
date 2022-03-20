e26a41b6cd18df0bca3df39c4ed5fa50695d1b2c
luftedar <ugurorcun91@gmail.com>
2022-03-20 16:49:42 +0300

    Add Validations for Expense

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :expense
+
+  validates :name, :amount, presence: true
+  validates :amount, numericality: { greater_than: 0 }
 end
