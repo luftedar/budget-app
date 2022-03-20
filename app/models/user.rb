f3571596c49f75dc783ea308366a6e3edaa76cf7
luftedar <ugurorcun91@gmail.com>
2022-03-20 16:53:35 +0300

    Fix Typeo in User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :groups, dependent: :destroy
-  has_many :expenses, dependent: :destory
+  has_many :expenses, dependent: :destroy
 
   validates :name, :email, presence: true
   validates :email, uniqueness: true
