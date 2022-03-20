4f10d2bf689ceea847392583b6f346d8c996ba96
luftedar <ugurorcun91@gmail.com>
2022-03-20 15:11:47 +0300

    Edit Devise Migration as self.up

--- db/migrate/20220320120819_devise_create_users.rb
+++ db/migrate/20220320120819_devise_create_users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
-  def change
+  def self.up
     change_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
