86f8442b8db3a050f2cb7433d19091f87db09af2
luftedar <ugurorcun91@gmail.com>
2022-03-21 19:02:18 +0300

    Add Sign Out Process

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root to: "groups#index"
   resources :users, only: [:index]
   resources :groups, only: %i[index show new create destroy] do
