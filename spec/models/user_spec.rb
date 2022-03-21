b5594e5c87c6f08b664d800cc96d87f2bd275414
luftedar <ugurorcun91@gmail.com>
2022-03-21 18:30:30 +0300

    Add Tests for User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject do
+    @test_user = User.new(name: 'testuser', email: 'test@test.com', password: 'testpass')
+  end
+
+  before { subject.save }
+
+  it 'Validates Provided Subject' do
+    expect(subject).to be_valid
+  end
+
+  it 'Validates Wrong Given User Name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Validates Wrong Given Email' do
+    subject.email = '1'
+    expect(subject).to_not be_valid
+  end
 end
