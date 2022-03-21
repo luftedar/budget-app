86dccb5473e31eaa33504c3d96c693c6a82f1089
luftedar <ugurorcun91@gmail.com>
2022-03-21 18:42:49 +0300

    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "groups#index"
+  resources :users, only: [:index]
+  resources :groups, only: %i[index show new create destroy] do
+    resources :expenses, only: %i[index show new create destroy]
+  end
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
