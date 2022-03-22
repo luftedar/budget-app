a62e06ba2ee85032539cada4b844cb23f6c6a5e6
luftedar <ugurorcun91@gmail.com>
2022-03-22 12:40:01 +0300

    Add Index Action for Expenses Controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,7 @@
 class ExpensesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
+    @expense = @group.expenses.all.order(created_at: :desc)
   end
 
   def new
