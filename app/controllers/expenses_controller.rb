b3e1ea421417de12644674190c4df2089ae6bdcd
luftedar <ugurorcun91@gmail.com>
2022-03-22 12:52:06 +0300

    Add Create Action for Expenses Controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -10,5 +10,13 @@ class ExpensesController < ApplicationController
   end
 
   def create
+    @group = Group.find(params[:group_id])
+    @expense = @group.expense.create(name: params[:name], amount: params[:amount], user_id: current_user.id, group_id: @group.id)
+
+    if @expense.save
+      flash[:success] = 'Expense Created Successfuly'
+    else
+      flash[:error] = 'Failed to Create Expense'
+    end
   end
 end
