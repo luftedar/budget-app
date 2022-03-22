84997b8d6e7068e660107e78e51a82a9d5bb8359
luftedar <ugurorcun91@gmail.com>
2022-03-22 12:02:05 +0300

    Update New & Create Actions for Groups Controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,8 +8,19 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    @group.user_id = current_user.id
+
+    if @group.save
+      flash[:notice] = 'Group Created Successfully'
+      redirect_to @group
+    else
+      flash[:notice] = 'Failed to Create Group'
+      render 'new'
+    end
   end
 end
