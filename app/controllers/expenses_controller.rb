2ecf5168c87d337d8b7db5bab89aaa1377dfab36
luftedar <ugurorcun91@gmail.com>
2022-03-22 12:55:54 +0300

    Add Protected Method for Create Expenses Actions

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -10,13 +10,20 @@ class ExpensesController < ApplicationController
   end
 
   def create
-    @group = Group.find(params[:group_id])
-    @expense = @group.expense.create(name: params[:name], amount: params[:amount], user_id: current_user.id, group_id: @group.id)
+    @group = Group.find(expense_params[:group_id])
+    @expense = @group.expense.create(name: expense_params[:name], amount: expense_params[:amount], user_id: current_user.id, group_id: @group.id)
 
     if @expense.save
       flash[:success] = 'Expense Created Successfuly'
+      redirect_to groups_path
     else
       flash[:error] = 'Failed to Create Expense'
     end
+  end
+
+  private
+
+  def expense_params 
+    params.require(:expense).permit(:name, :amount)
   end
 end
