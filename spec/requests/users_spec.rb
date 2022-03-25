d3ea73d62b4ee71190af86c65e496a4f6ac1b1bb
luftedar <ugurorcun91@gmail.com>
2022-03-25 12:38:06 +0300

    Test for User Controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_index_path
-      expect(response).to have_http_status(200)
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    before(:example) { get root_path }
+
+    it 'Has HTTP Status OK' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'Should Include Correct Header' do
+      expect(response.body).to include('budgeTracker')
     end
   end
 end
