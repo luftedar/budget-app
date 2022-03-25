f70744ea936523732aa3924119db50d6e01b3e3d
luftedar <ugurorcun91@gmail.com>
2022-03-25 15:14:48 +0300

    Implement Tests for New Group

--- spec/views/groups/new.html.erb_spec.rb
+++ spec/views/groups/new.html.erb_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'groups/new.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create!(name: 'testuser', email: 'test@testmail.com', password: 'testpassword', password_confirmation: 'testpassword')
+    @group = @user.groups.create!(name: 'testgroup', icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Video-Game-Controller-Icon-IDV-green.svg/2048px-Video-Game-Controller-Icon-IDV-green.svg.png', user_id: @user.id)
+    visit user_session_path
+    fill_in 'Email', with: 'test@testmail.com'
+    fill_in 'Password', with: 'testpassword'
+    click_button 'Log in'
+    click_link('Add New Category')
+  end
+  it 'Validates HTML Elements' do
+    expect(page).to have_content('Name')
+    expect(page).to have_content('Icon')
+  end
+  it 'Renders Save Button' do
+    expect(page).to have_button 'Save'
+  end
 end
